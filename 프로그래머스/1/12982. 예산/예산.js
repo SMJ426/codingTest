b7956cfbc224dbd17ec9089dccccf82d57b7085d
SMJ426 <122437649+SMJ426@users.noreply.github.com>
2024-02-19 22:57:36 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,13 @@
+function solution(d, budget) {
+    let sum =0;
+    let answer = 0;
+    
+    d.sort((a,b)=>a-b)
+    
+    for(let i=0; i < d.length; i++){
+        answer ++;
+        sum += d[i];
+        if(sum > budget) answer--;
+    }
+    return answer;
+}
