0a28109915372c90fce3c95c81ec932253608140
SMJ426 <122437649+SMJ426@users.noreply.github.com>
2024-04-10 22:20:34 +0900

    [level 2] Title: 전화번호 목록, Time: 399.59 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,11 +1,10 @@
 function solution(phone_book) {
-  phone_book.sort(); 
-
-  for (let i = 0; i < phone_book.length - 1; i++) {
-    if (phone_book[i] === phone_book[i + 1].substring(0, phone_book[i].length)) {
-      return false;
+    phone_book.sort(); 
+    
+    for(let i=0 ; i< phone_book.length-1 ; i++){
+        if(phone_book[i]===phone_book[i+1].substring(0,phone_book[i].length))
+            return false;
+        
     }
-  }
-
-  return true; 
+    return true;
 }
