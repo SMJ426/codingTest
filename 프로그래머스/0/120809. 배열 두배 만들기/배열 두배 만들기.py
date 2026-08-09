7556828c515391a94a9e0b2e3b797c0ee31bffac
Minjae Shin <122437649+SMJ426@users.noreply.github.com>
2026-08-09 16:03:07 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.07 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    answer = []
+    for n in numbers:
+        answer.append(n * 2)
+    return answer
+
+# def solution(numbers):
+#     answer = [n * 2 for n in numbers]
+#     return answer
